7773899003895122c1851b5fa24c70245211032c
JOETION <1322874562@qq.com>
2019-06-03 21:58:48 +0800

    细微修改，添加部分注释

--- src/main/java/com/qexz/Application.java
+++ src/main/java/com/qexz/Application.java
@@ -9,9 +9,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.util.ClassUtils;
 
 import java.util.Date;
 
+/**
+ * <p>返回方法最好用接口</p>
+ * <p>Java 8新的语法，Lambda表达式，返回一个方法。在之前的版本中，返回方法一般用返回一个匿名的接口的实现类实现。</p>
+ * 参考网址：<a>https://ask.csdn.net/questions/198070?sort=votes_count</a>
+ */
 @SpringBootApplication
 @EnableTransactionManagement
 @ServletComponentScan
@@ -37,16 +43,17 @@ public class Application {
         };
     }
 
+    //这里是什么语法？
     @Bean
     public EmbeddedServletContainerCustomizer containerCustomizer() {
 
         return (container -> {
             //ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
-            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
-            //ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/error/404.html");
+            ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error/500.html");
 
             //container.addErrorPages(error401Page, error404Page, error500Page);
-            container.addErrorPages(error404Page);
+            container.addErrorPages(error404Page, error500Page);
         });
     }
 
--- src/main/java/com/qexz/config/QexzWebMvcConfigurerAdapter.java
+++ src/main/java/com/qexz/config/QexzWebMvcConfigurerAdapter.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+
+/**
+ * <p>此类在本项目中没有作用，该项目中静态资源用Nginx管理，此处留作学习使用<p/>
+ */
 @Configuration
 public class QexzWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
 
@@ -14,6 +18,7 @@ public class QexzWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
 
     /**
      * 配置静态访问资源
+     *
      * @param registry
      */
     @Override
